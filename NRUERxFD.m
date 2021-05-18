ed3287901399ccc7ad24e2e78484a9b9b805e75d
Kubilay Ulger <70947681+okulger@users.noreply.github.com>
2021-05-18 15:37:32 -0400

    Add files via upload

--- NRUERxFD.m
+++ NRUERxFD.m
@@ -7,7 +7,7 @@ classdef NRUERxFD < matlab.System
         waveformConfig;  % Waveform config
         
         % OFDM grid
-        rxGrid;
+ %       rxGrid;
         
         % Transport block data for last transmission
         targetCodeRate = 490/1024;  % Target code rate
@@ -50,72 +50,79 @@ classdef NRUERxFD < matlab.System
     methods (Access = protected)
         
         
-        function stepImpl(obj, rxGrid, chanGrid, noiseVar)
-            % Demodulates and decodes one slot of data
-            chanGrid = permute(chanGrid,[4,3,1,2]);
-            rxGrid = permute(rxGrid,[3,2,1]);
-            % Get PDSCH received symbols and channel estimates
-            % from received grid 
-            [pdschInd,pdschInfo] = nrPDSCHIndices(...
-                obj.carrierConfig, obj.pdschConfig);
-            [pdschRx, pdschHest] = nrExtractResources(pdschInd, rxGrid,...
-                chanGrid);
-            
-            % TODO:  Perform the MMSE equalization using the
-            % nrEqualizeMMSE() function.
-            % Use the PDSCH Rx symbols, PDSCH channel estimate and noise
-            % variance as the input.  Store the equalized symbols in
-            % obj.pdschEq and  channel state information in a structure,
-            % csi.            
-            %    [obj.pdschEq,csi] = nrEqualizeMMSE(...);
-            [obj.pdschEq,csi] = nrEqualizeMMSE(pdschRx,pdschHest,noiseVar);
-                        
-            % TODO:  Get the LLRs with the nrPDSCHDecode() function.
-            % Use carrier and PDSCH configuration, the equalized symbols,
-            % and the noise variance, noiseVar.
-            %    [obj.pdschEq,csi] = nrEqualizeMMSE(...);   
-            [dlschLLRs,rxSym] = nrPDSCHDecode(obj.carrierConfig,obj.pdschConfig,obj.pdschEq,noiseVar);
-            % Scale LLRs by EbN0.  
-            % The csi value computed in the nrEqualizeMMSE()
-            % function is csi = |pdschHest|^2 + noiseVar.
-            % Also, the Eb/N0 = snrEq/Qm where Qm is the number of bits 
-            % per symbol and snrEq is the SNR after equalization,
-            %
-            %   snrEq = (|pdschHest|^2 + noiseVar)/noiseVar = csi/noiseVar
-            %
-            % Hence, Eb/N0 = csi/(noiseVar*Qm).
-            % Since the LLRs from the nrPDSCHDecode function are 
-            % already scaled by 1/noiseVar, we multiply them by  csi/Qm.
-            csi = nrLayerDemap(csi); % CSI layer demapping
-            numCW = length(csi);
-            for cwIdx = 1:numCW
-                Qm = length(dlschLLRs{cwIdx})/length(rxSym{cwIdx}); % bits per symbol
-                csi{cwIdx} = repmat(csi{cwIdx}.',Qm,1);   % expand by each bit per symbol
-                dlschLLRs{cwIdx} = dlschLLRs{cwIdx} .* csi{cwIdx}(:);   % scale
+        function stepImpl(obj, rxGrid3D, chanGrid4D, noiseVar2D, nStreams)
+      
+            for i=1:nStreams
+                % Demodulates and decodes one slot of data
+                chanGrid = squeeze(chanGrid4D(i,i,:,:));
+                rxGrid = squeeze(rxGrid3D(i,:,:));
+                noiseVar = noiseVar2D(i);
+                % Get PDSCH received symbols and channel estimates
+                % from received grid 
+                [pdschInd,pdschInfo] = nrPDSCHIndices(...
+                    obj.carrierConfig, obj.pdschConfig);
+                [pdschRx, pdschHest] = nrExtractResources(pdschInd, rxGrid,...
+                    chanGrid);
+
+                % TODO:  Perform the MMSE equalization using the
+                % nrEqualizeMMSE() function.
+                % Use the PDSCH Rx symbols, PDSCH channel estimate and noise
+                % variance as the input.  Store the equalized symbols in
+                % obj.pdschEq and  channel state information in a structure,
+                % csi.            
+                %    [obj.pdschEq,csi] = nrEqualizeMMSE(...);
+                [obj.pdschEq,csi] = nrEqualizeMMSE(pdschRx,pdschHest,noiseVar);
+
+                % TODO:  Get the LLRs with the nrPDSCHDecode() function.
+                % Use carrier and PDSCH configuration, the equalized symbols,
+                % and the noise variance, noiseVar.
+                %    [obj.pdschEq,csi] = nrEqualizeMMSE(...);   
+                [dlschLLRs,rxSym] = nrPDSCHDecode(obj.carrierConfig,obj.pdschConfig,obj.pdschEq,noiseVar);
+                % Scale LLRs by EbN0.  
+                % The csi value computed in the nrEqualizeMMSE()
+                % function is csi = |pdschHest|^2 + noiseVar.
+                % Also, the Eb/N0 = snrEq/Qm where Qm is the number of bits 
+                % per symbol and snrEq is the SNR after equalization,
+                %
+                %   snrEq = (|pdschHest|^2 + noiseVar)/noiseVar = csi/noiseVar
+                %
+                % Hence, Eb/N0 = csi/(noiseVar*Qm).
+                % Since the LLRs from the nrPDSCHDecode function are 
+                % already scaled by 1/noiseVar, we multiply them by  csi/Qm.
+                csi = nrLayerDemap(csi); % CSI layer demapping
+                numCW = length(csi);
+                for cwIdx = 1:numCW
+                    Qm = length(dlschLLRs{cwIdx})/length(rxSym{cwIdx}); % bits per symbol
+                    csi{cwIdx} = repmat(csi{cwIdx}.',Qm,1);   % expand by each bit per symbol
+                    dlschLLRs{cwIdx} = dlschLLRs{cwIdx} .* csi{cwIdx}(:);   % scale
+                end
+
+                % Compute the extra overhead from the PT-RS
+                Xoh_PDSCH = 6*obj.pdschConfig.EnablePTRS;
+
+                % Calculate the transport block size based on the PDSCH
+                % allocation and target code rate
+                obj.trBlkSizes = nrTBS(obj.pdschConfig.Modulation,obj.pdschConfig.NumLayers,...
+                    numel(obj.pdschConfig.PRBSet),pdschInfo.NREPerPRB,...
+                    obj.targetCodeRate,Xoh_PDSCH);
+                obj.decDLSCH.TransportBlockLength = obj.trBlkSizes;
+
+                % Reset the soft buffer
+                harqId = 0;
+                obj.decDLSCH.resetSoftBuffer(harqId);
+
+                % TODO:  Decode the bits with the obj.decDLSCH() method.
+                % Use the scaled LLRs from above. Use a redundancy version, 
+                % rv = 0,  since we are not using HARQ in this lab.
+                %    rv = 0;
+                %    obj.rxBit = obj.decDLSCH(...);
+    %             rv = [0,0];
+                rv=0;
+                %Get bits in this layer
+                newBits = obj.decDLSCH(dlschLLRs,obj.pdschConfig.Modulation,obj.pdschConfig.NumLayers,rv);
+                %Add it to rest
+                obj.rxBits =[obj.rxBits; newBits];
             end
-                    
-            % Compute the extra overhead from the PT-RS
-            Xoh_PDSCH = 6*obj.pdschConfig.EnablePTRS;
-            
-            % Calculate the transport block size based on the PDSCH
-            % allocation and target code rate
-            obj.trBlkSizes = nrTBS(obj.pdschConfig.Modulation,obj.pdschConfig.NumLayers,...
-                numel(obj.pdschConfig.PRBSet),pdschInfo.NREPerPRB,...
-                obj.targetCodeRate,Xoh_PDSCH);
-            obj.decDLSCH.TransportBlockLength = obj.trBlkSizes;
-                         
-            % Reset the soft buffer
-            harqId = 0;
-            obj.decDLSCH.resetSoftBuffer(harqId);
-            
-            % TODO:  Decode the bits with the obj.decDLSCH() method.
-            % Use the scaled LLRs from above. Use a redundancy version, 
-            % rv = 0,  since we are not using HARQ in this lab.
-            %    rv = 0;
-            %    obj.rxBit = obj.decDLSCH(...);
-%             rv = [0,0];
-            rv=0;
-            obj.rxBits = obj.decDLSCH(dlschLLRs,obj.pdschConfig.Modulation,obj.pdschConfig.NumLayers,rv);
         end
         
     end
--- NRgNBTxFD.m
+++ NRgNBTxFD.m
@@ -14,7 +14,6 @@ classdef NRgNBTxFD < matlab.System
                
         % DLSCH encoder
         encDLSCH;
-        nant;
         
         
     end
@@ -40,56 +39,60 @@ classdef NRgNBTxFD < matlab.System
     end
     methods (Access = protected)
                
-        function txGrid = stepImpl(obj)
-            % step implementation. Creates one slot of samples for each
-            % component carrier
-            
-            
-            % Create the OFDM grid representing the array of modulation
-            % symbols to be transmitted
-            txGrid = nrResourceGrid(obj.carrierConfig, ...
-                obj.pdschConfig.NumLayers);
-            
-                       
-            % Get indices on where the PDSCH is allocated
-            [pdschInd,pdschInfo] = nrPDSCHIndices(obj.carrierConfig, obj.pdschConfig);
-            
-            % Compute the extra overhead from the PT-RS
-            Xoh_PDSCH = 6*obj.pdschConfig.EnablePTRS;     
-            
-            % Calculate the transport block size based on the PDSCH
-            % allocation and target code rate
-            obj.trBlkSizes = nrTBS(obj.pdschConfig.Modulation,obj.pdschConfig.NumLayers,...
-                numel(obj.pdschConfig.PRBSet),pdschInfo.NREPerPRB,...
-                obj.targetCodeRate,Xoh_PDSCH);
-            
-            % Generate random bits for each codeword and set the transport
-            % block
-            obj.txBits = cell(obj.pdschConfig.NumCodewords, 1);
-            for icw = 1:obj.pdschConfig.NumCodewords
+        function txGrid3D = stepImpl(obj,nStreams)
+            txGrid3D=zeros([nStreams,1,732,14]);
+            for i=1:nStreams
+                % step implementation. Creates one slot of samples for each
+                % component carrier
 
-                % Create random bits
-                obj.txBits{icw} = randi([0 1], obj.trBlkSizes(icw), 1);
+
+                % Create the OFDM grid representing the array of modulation
+                % symbols to be transmitted
+                txGrid = nrResourceGrid(obj.carrierConfig, ...
+                    obj.pdschConfig.NumLayers);
+
+
+                % Get indices on where the PDSCH is allocated
+                [pdschInd,pdschInfo] = nrPDSCHIndices(obj.carrierConfig, obj.pdschConfig);
+
+                % Compute the extra overhead from the PT-RS
+                Xoh_PDSCH = 6*obj.pdschConfig.EnablePTRS;     
+                % Calculate the transport block size based on the PDSCH
+                % allocation and target code rate
+                obj.trBlkSizes = nrTBS(obj.pdschConfig.Modulation,obj.pdschConfig.NumLayers,...
+                    numel(obj.pdschConfig.PRBSet),pdschInfo.NREPerPRB,...
+                    obj.targetCodeRate,Xoh_PDSCH);
+
+                % Generate random bits for each codeword and set the transport
+                % block
+                BitsPerLayer = cell(obj.pdschConfig.NumCodewords, 1);
+                for icw = 1:obj.pdschConfig.NumCodewords
+
+                    % Create random bits
+                    BitsPerLayer{icw} = randi([0 1], obj.trBlkSizes(icw), 1);
+
+                    % Encode data                
+                    obj.encDLSCH.setTransportBlock(BitsPerLayer{icw},icw-1);
+                end
+
+                % Encode the DL-SCH transport blocks.  We use a redundancy
+                % version (rv) = 0 since we are not simulating HARQ now.
+                rv=0;
+                codedTrBlock = obj.encDLSCH(obj.pdschConfig.Modulation, ...
+                    obj.pdschConfig.NumLayers, pdschInfo.G, rv);
+
+                % Modulate the PDSCH modulation
+                pdschSymbols = nrPDSCH(obj.carrierConfig, obj.pdschConfig, ...
+                    codedTrBlock);
+
+                % Map the modulated symbols to the OFDM grid
+                txGrid(pdschInd) = pdschSymbols;  
+                txGrid3D(i,1,:,:)=txGrid;
                 
-                % Encode data                
-                obj.encDLSCH.setTransportBlock(obj.txBits{icw},icw-1);
+                % Adding up the bits - CHANGE BY KUBI
+                obj.txBits = [obj.txBits ; BitsPerLayer];
+                
             end
-            
-            % Encode the DL-SCH transport blocks.  We use a redundancy
-            % version (rv) = 0 since we are not simulating HARQ now.
- %           rv = [0,0];
-            rv=0;
-            codedTrBlock = obj.encDLSCH(obj.pdschConfig.Modulation, ...
-                obj.pdschConfig.NumLayers, pdschInfo.G, rv);
-            
-            % Modulate the PDSCH modulation
-            pdschSymbols = nrPDSCH(obj.carrierConfig, obj.pdschConfig, ...
-                codedTrBlock);
-          
-
-
-            % Map the modulated symbols to the OFDM grid
-            txGrid(pdschInd) = pdschSymbols;   
         end
         
     end
